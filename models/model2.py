bb30a14b976177275a7f369c7e6c4f63e55450f4
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-02-04 20:32:22 +0200

    repo management and model improvements

--- models/model2.py
+++ models/model2.py
@@ -2,33 +2,35 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import SMOTE,RandomOverSampler
 import joblib as jb
 import pandas as pd
 
 food = pd.read_csv('food.csv')
+food['Take-aways'] = food[["Meal, Inexpensive Restaurant","Meal for 2 People, Mid-range Restaurant, Three-course","McMeal at McDonalds (or Equivalent Combo Meal)"]].sum(axis=1)
+food['Drinks'] = food[["Coke/Pepsi (0.33 liter bottle)","Water (0.33 liter bottle) ","Water (1.5 liter bottle)","Cappuccino (regular)"]].sum(axis=1)
+food['Dairy and Wheat'] = food[["Milk (regular), (1 liter)","Loaf of Fresh White Bread (500g)","Eggs (regular) (12)","Local Cheese (1kg)"]].sum(axis=1)
+food['Fruits and Vegetables'] = food[["Apples (1kg)","Oranges (1kg)","Potato (1kg)","Lettuce (1 head)","Rice (white), (1kg)","Tomato (1kg)","Banana (1kg)","Onion (1kg)"]].sum(axis=1)
+food['Meat'] = food[["Chicken Breasts (Boneless, Skinless), (1kg)","Beef Round (1kg) (or Equivalent Back Leg Red Meat)"]].sum(axis=1)
+food = food[["Take-aways","Drinks","Dairy and Wheat","Fruits and Vegetables","Meat","Country","Capital"]]
 x = food.drop(['Country','Capital'],axis=1)
+
 def categorize_quantiles(df):
     result = pd.DataFrame(index=df.index)
     for col in df.columns:
-        quantiles = df[col].quantile([0.0,0.13,0.25, 0.5,0.38, 0.75,1.0])
-        q1, q2, q3,q4,q5,q6,q7 = quantiles[0.0],quantiles[0.13],quantiles[0.25], quantiles[0.5], quantiles[0.38], quantiles[0.75], quantiles[1.00]
+        quantiles = df[col].quantile([0.25, 0.5, 0.75])
+        q1, q2, q3 = quantiles[0.25], quantiles[0.5], quantiles[0.75]
         col_categories = []
         for val in df[col]:
             if val < q1:
-                col_categories.append("very very cheap")
-            elif val >= q1 and val < q2:
-                col_categories.append("very cheap")
-            elif val >= q3 and val < q4:
                 col_categories.append("cheap")
-            elif val >= q4 and val < q5:
+            elif val >= q1 and val < q2:
                 col_categories.append("medium")
-            elif val >= q5 and val < q6:
+            elif val >= q2 and val < q3:
                 col_categories.append("expensive")
-            elif val >= q6 and val < q7:
-                col_categories.append("very expensive")
             else:
-                col_categories.append("very very expensive")
+                col_categories.append("very expensive")
         result[col + '_category'] = col_categories
     return result
 f = categorize_quantiles(x)
@@ -50,7 +52,7 @@ ros = RandomOverSampler(random_state=123)
 x_re, y_re = ros.fit_resample(x_new, y_new)
 sm = SMOTE()
 x_smo ,y_smo = sm.fit_resample(x_re,y_re)
-new_model = KNeighborsClassifier(n_neighbors=1)
+new_model = RandomForestClassifier(random_state=123,n_estimators=100)
 new_model.fit(x_smo,y_smo)
-jb.dump(new_model,"food_model.joblib")
+jb.dump(new_model,"food_model2.joblib")
 print("model saved successfully")
